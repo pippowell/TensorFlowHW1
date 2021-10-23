44f47df23666b39a0af6026940a21cab9aaea6bd
pippowell <93034705+pippowell@users.noreply.github.com>
2021-10-23 12:17:20 +0200

    Add files via upload

---
+++ cat.py
@@ -0,0 +1,12 @@
+class CuteCat:
+    def __init__(self, name):
+        self.name = name
+        
+    def greet(self, name2):
+        print ("Hi, my name is " + self.name         + ". I am a cute cuddly cat. I see you are too, " + name2         + ". Let's purr so the two-leggeds will give us food.")
+        
+    def respond(self, name2):
+        print ("Hi, " + name2 + ". That sounds like a great idea. Let's do it because I'm hungry.")
+        
+    
+
---
+++ kittyconcert.py
@@ -0,0 +1,5 @@
+import cat
+cat1 = cat.CuteCat("Harry")
+cat2 = cat.CuteCat("Snowbell")
+cat1.greet(cat2.name)
+cat2.respond(cat1.name)
